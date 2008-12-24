caba1b1f9a9bb7b5d10bcc58aee5226f74492c6a
hovik.melikyan <hovik.melikyan@0ff1fb3a-c966-11dd-86c2-0f8696b7b6f9>
2008-12-24 22:48:15 +0000

    Commit early, commit often.

--- src/main.cpp
+++ src/main.cpp
@@ -85,15 +85,15 @@ public:
     void  pushInt(int v)      { push().int_ = v; }
     void  pushPtr(ptr v)      { push().ptr_ = v; }
     int   popInt()            { return pop().int_; }
-    int   replInt(int v)      { return stack.top().int_ = v; }
     ptr   popPtr()            { return pop().ptr_; }
-    int   topInt()            { return stack.top().int_; }
-    ptr   topPtr()            { return stack.top().ptr_; }
+    int   topInt() const      { return stack.top().int_; }
+    ptr   topPtr() const      { return stack.top().ptr_; }
+    int&  topIntRef()         { return stack.top().int_; }
 
 #ifdef PTR64
-    void  pushLarge(large v)  { push().large_ = v; }
-    large popLarge()          { return pop().large_; }
-    large topLarge()          { return stack.top().large_; }
+    void   pushLarge(large v) { push().large_ = v; }
+    large  popLarge()         { return pop().large_; }
+    large& topLarge()         { return stack.top().large_; }
 #else
     void  pushLarge(large v)
         { push().int_ = int(v); push().int_ = int(v >> 32); }
@@ -185,6 +185,15 @@ static int compareInt(int a, int b)
 static int compareLarge(large a, large b)
     { if (a < b) return -1; else if (a == b) return 0; return 1; }
 
+static int compareStr(ptr a, ptr b)
+    { return PTR_TO_STRING(a).compare(PTR_TO_STRING(b)); }
+
+static int compareStrChr(ptr a, int b)
+    { return PTR_TO_STRING(a).compare(string(char(b))); }
+
+static int compareChrStr(int a, ptr b)
+    { return string(char(a)).compare(PTR_TO_STRING(b)); }
+
 
 void VmCode::run(VmQuant* p)
 {
@@ -226,13 +235,15 @@ void VmCode::run(VmQuant* p)
         case opCmpInt:
             {
                 int r = vmStack.popInt();
-                vmStack.replInt(compareInt(vmStack.topInt(), r));
+                int& t = vmStack.topIntRef();
+                t = compareInt(t, r);
             }
             break;
         case opCmpIntLarge:
             {
                 large r = vmStack.popLarge();
-                vmStack.replInt(compareLarge(vmStack.topInt(), r));
+                int& t = vmStack.topIntRef();
+                t = compareLarge(t, r);
             }
             break;
         case opCmpLarge:
@@ -249,17 +260,33 @@ void VmCode::run(VmQuant* p)
                 vmStack.pushInt(compareLarge(l, r));
             }
             break;
-/*
-    opCmpStr,       // [opcmp]           -2 +1
-    opCmpStrChr,    // [opcmp]           -2 +1
-    opCmpChrStr,    // [opcmp]           -2 +1
-*/
-        case opEQ: vmStack.replInt(vmStack.topInt() == 0); break;
-        case opLT: vmStack.replInt(vmStack.topInt() < 0); break;
-        case opLE: vmStack.replInt(vmStack.topInt() <= 0); break;
-        case opGE: vmStack.replInt(vmStack.topInt() >= 0); break;
-        case opGT: vmStack.replInt(vmStack.topInt() > 0); break;
-        case opNE: vmStack.replInt(vmStack.topInt() != 0); break;
+        case opCmpStr:
+            {
+                ptr r = vmStack.popPtr();
+                ptr l = vmStack.popPtr();
+                vmStack.pushInt(compareStr(l, r));
+            }
+            break;
+        case opCmpStrChr:
+            {
+                int r = vmStack.popInt();
+                ptr l = vmStack.popPtr();
+                vmStack.pushInt(compareStrChr(l, r));
+            }
+            break;
+        case opCmpChrStr:
+            {
+                ptr r = vmStack.popPtr();
+                int& t = vmStack.topIntRef();
+                t = compareChrStr(t, r);
+            }
+            break;
+        case opEQ: { int& t = vmStack.topIntRef(); t = t == 0; } break;
+        case opLT: { int& t = vmStack.topIntRef(); t = t < 0; } break;
+        case opLE: { int& t = vmStack.topIntRef(); t = t <= 0; } break;
+        case opGE: { int& t = vmStack.topIntRef(); t = t >= 0; } break;
+        case opGT: { int& t = vmStack.topIntRef(); t = t > 0; } break;
+        case opNE: { int& t = vmStack.topIntRef(); t = t != 0; } break;
         default: fatal(CRIT_FIRST + 50, ("[VM] Unknown opcode " + itostring((--p)->op_, 16, 8, '0')).c_str());
         }
     }
@@ -392,12 +419,9 @@ void VmCode::genComparison(OpCode cmp)
         else if (rightStr)
             op = opCmpStr;
     }
-    else if (rightStr)
+    else if (rightStr && left->isChar())
     {
-        if (left->isChar())
-            op = opCmpStrChr;
-        else if (leftStr)
-            op = opCmpStr;
+        op = opCmpChrStr;
     }
 
     else if (left->isOrdinal() && right->isOrdinal())
@@ -813,9 +837,9 @@ int main()
         initLangObjs();
         
 #ifdef XCODE
-        ShModule module("../../src/tests/test.sn");
+        ShModule module("../../src/z.sn");
 #else
-        ShModule module("tests/test.sn");
+        ShModule module("z.sn");
 #endif
         module.compile();
         
