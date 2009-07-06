86f85703746794665a5db14c57cc461aff00fe9c
hovik.melikyan <hovik.melikyan@0ff1fb3a-c966-11dd-86c2-0f8696b7b6f9>
2009-07-06 13:27:27 +0000

    Moved atomic functions to common.cpp

--- trunk/src/common.cpp
+++ trunk/src/common.cpp
@@ -27,3 +27,34 @@ int memquantize(int a)
 }
 
 
+
+#ifndef SINGLE_THREADED
+
+#if defined(__GNUC__) && (defined(__i386__) || defined(__I386__)|| defined(__x86_64__))
+// multi-threaded version with GCC on i386
+
+
+int pincrement(int* target)
+{
+    int temp = 1;
+    __asm__ __volatile ("lock ; xaddl %0,(%1)" : "+r" (temp) : "r" (target));
+    return temp + 1;
+}
+
+
+int pdecrement(int* target)
+{
+    int temp = -1;
+    __asm__ __volatile ("lock ; xaddl %0,(%1)" : "+r" (temp) : "r" (target));
+    return temp - 1;
+}
+
+
+#else
+
+#error Undefined architecture: atomic functions are not available
+
+#endif
+
+#endif
+
